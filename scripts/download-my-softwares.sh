7a7693dd6442392da37420b7b8a732dbb8639925
Elton John <37675998+EltonBR@users.noreply.github.com>
2021-03-21 02:22:55 -0300

    Update download-my-softwares.sh

--- scripts/download-my-softwares.sh
+++ scripts/download-my-softwares.sh
@@ -2,7 +2,7 @@
 sudo apt install gitk meld mariadb-server php8.0-mysql php8.0-gd  php8.0-zip php8.0-curl php8.0-mbstring php8.0-xml php8.0 build-essential cmake make sublime-text git openjdk-8-jdk-headless openjdk-8-jre deluge mysql-workbench filezilla python3.7 -y
 
 #tools
-sudo apt install openssh-server wine-stable wireshark remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap qemu gparted deluge google-chrome-stable aria2 pdfimages curl -y
+sudo apt install minicom openssh-server wine-stable wireshark remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap qemu gparted deluge google-chrome-stable aria2 pdfimages curl -y
 
 #virtualization
 sudo apt install virtualbox qemu -y
