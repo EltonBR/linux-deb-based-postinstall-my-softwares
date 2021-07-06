8fdcb1d39f909ede01eaf0a78deeef062331ee18
Elton John <37675998+EltonBR@users.noreply.github.com>
2021-07-06 14:55:55 -0300

    Update download-my-softwares.sh

--- scripts/download-my-softwares.sh
+++ scripts/download-my-softwares.sh
@@ -1,5 +1,5 @@
 # dev 
-sudo apt install gitk meld mariadb-server php8.0-mysql php8.0-gd  php8.0-zip php8.0-curl php8.0-mbstring php8.0-xml php8.0 build-essential cmake make sublime-text git openjdk-8-jdk-headless openjdk-8-jre deluge mysql-workbench filezilla python3.7 -y
+sudo apt install gitk meld build-essential cmake make sublime-text git openjdk-8-jdk deluge mysql-workbench filezilla python3.7 -y
 
 #tools
 sudo apt install minicom openssh-server wine-stable wireshark remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap qemu gparted deluge google-chrome-stable aria2 pdfimages curl -y
@@ -14,7 +14,7 @@ sudo apt install blender sweethome3d sweethome3d-furniture inkscape krita gimp -
 sudo apt install vlc kazam openshot ffmpeg -y
 
 #gnome
-sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-panel -y
+sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu -y
 
 
 #cli tools
