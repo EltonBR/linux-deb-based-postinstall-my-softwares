22597c44db3c1d896ed2089420266cbba765761b
Elton John <37675998+EltonBR@users.noreply.github.com>
2021-07-06 14:52:31 -0300

    Update install-dotnetcore-ubuntu.sh

--- scripts/install-dotnetcore-ubuntu.sh
+++ scripts/install-dotnetcore-ubuntu.sh
@@ -1,5 +1,7 @@
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo apt-get install -y apt-transport-https
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1 dotnet-sdk-5.0
+#wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+#sudo apt-get install -y apt-transport-https
+#sudo dpkg -i packages-microsoft-prod.deb
+#sudo apt-get update
+#sudo apt-get install -y dotnet-sdk-3.1 dotnet-sdk-5.0
+
+#TODO: remove script
