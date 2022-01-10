f7e31c5e6059686a01e73bc3bd6254ebc7612e79
Elton John <37675998+EltonBR@users.noreply.github.com>
2022-01-10 10:34:26 -0300

    Update set-ppa.sh

--- scripts/set-ppa.sh
+++ scripts/set-ppa.sh
@@ -4,6 +4,5 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt-get install software-properties-common -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo add-apt-repository ppa:ondrej/php
 sudo add-apt-repository ppa:thomas-schiex/blender
 sudo apt-get update
