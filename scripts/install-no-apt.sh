c378c51e95074bb73cdcae9386320e285aef7e5d
Elton John <37675998+EltonBR@users.noreply.github.com>
2022-06-20 21:48:10 -0300

    Update install-no-apt.sh

--- scripts/install-no-apt.sh
+++ scripts/install-no-apt.sh
@@ -1,4 +1,3 @@
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
