ae4de0b9f90b949941ba033116837b12c6479a3d
Elton John <37675998+EltonBR@users.noreply.github.com>
2022-06-20 21:47:05 -0300

    Update download-my-softwares.sh

--- scripts/download-my-softwares.sh
+++ scripts/download-my-softwares.sh
@@ -1,14 +1,14 @@
 # dev 
-sudo apt install gitk meld build-essential cmake make sublime-text git openjdk-8-jdk deluge python2.7 python3-venv filezilla python3.7 -y
+sudo apt install gitk meld build-essential cmake make sublime-text git deluge python2.7 python3-venv filezilla python3.7 -y
 
 #tools
-sudo apt install minicom openssh-server wine-stable wireshark remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap qemu gparted deluge google-chrome-stable aria2 pdfimages -y
+sudo apt install minicom openssh-server wine-stable wireshark remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap gparted deluge google-chrome-stable aria2 pdfimages -y
 
 #virtualization
 sudo apt install virtualbox qemu -y
 
 #image and 3D
-sudo apt install blender sweethome3d sweethome3d-furniture inkscape krita gimp -y
+sudo apt install blender sweethome3d sweethome3d-furniture inkscape krita -y
 
 #videos edit and player
 sudo apt install vlc kazam kdenlive ffmpeg -y
@@ -18,4 +18,4 @@ sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-pa
 
 
 #cli tools
-sudo apt install ranger ncdu curl -y
+sudo apt install ranger ncdu curl net-tools -y
