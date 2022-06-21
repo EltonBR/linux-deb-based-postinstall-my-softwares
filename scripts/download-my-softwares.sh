ca90c85ad13c6b07edd282894ac7dd41b7fc6cc3
Elton John <37675998+EltonBR@users.noreply.github.com>
2022-06-20 21:56:43 -0300

    Update download-my-softwares.sh

--- scripts/download-my-softwares.sh
+++ scripts/download-my-softwares.sh
@@ -1,5 +1,5 @@
 # dev 
-sudo apt install gitk meld build-essential cmake make sublime-text git deluge python2.7 python3-venv filezilla python3.7 -y
+sudo apt install gitk meld build-essential cmake make sublime-text git deluge python2.7 python3-venv filezilla python3 -y
 
 sudo apt install code -y
 
