4c6d63dae8b716f4818f6a9432b51ac6cba9bc41
Elton John <37675998+EltonBR@users.noreply.github.com>
2022-06-26 18:06:44 -0300

    Update download-my-softwares.sh

--- scripts/download-my-softwares.sh
+++ scripts/download-my-softwares.sh
@@ -1,10 +1,25 @@
-# dev 
-sudo apt install gitk meld build-essential cmake make sublime-text deluge python2.7 python3-venv filezilla python3 python3-distutils python3-venv -y
+#gnome
+sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu gnome-shell-extension -y
 
+#cli tools
+sudo apt install ranger ncdu curl net-tools -y
+
+#utils
+#sudo apt install filezilla -y
+sudo apt install delugue -y
+#py 
+sudo apt install python2.7 python3-venv python3 python3-distutils python3-venv -y
+
+# dev 
+sudo apt install gitk meld build-essential cmake make sublime-text -y
+
+#
+sudo apt install wireshark openssh-server -y
+#
 sudo apt install code google-chrome-stable git -y
 
 #tools
-sudo apt install minicom openssh-server wine-stable wireshark remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap gparted deluge aria2 pdfimages -y
+sudo apt install minicom remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap gparted deluge aria2 pdfimages -y
 
 #virtualization
 sudo apt install virtualbox qemu -y
@@ -16,8 +31,4 @@ sudo apt install blender sweethome3d sweethome3d-furniture inkscape krita -y
 sudo apt install vlc kazam kdenlive ffmpeg -y
 
 #gnome
-sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu -y
-
-
-#cli tools
-sudo apt install ranger ncdu curl net-tools -y
+sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu gnome-shell-extension -y
