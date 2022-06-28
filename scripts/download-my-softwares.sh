9cab4430a53b398bafa0706388872abb9be66ecd
Elton John <37675998+EltonBR@users.noreply.github.com>
2022-06-27 22:57:32 -0300

    Update download-my-softwares.sh

--- scripts/download-my-softwares.sh
+++ scripts/download-my-softwares.sh
@@ -3,14 +3,14 @@ sudo apt install git -y
 sudo apt install gitk meld build-essential cmake make -y
 
 #cli tools
-sudo apt install ranger ncdu curl net-tools -y
-
+sudo apt install ranger ncdu curl net-tools nmap tmux ne -y
+sudo apt install emacs -y
 #utils
 #sudo apt install filezilla -y
-sudo apt install delugue -y
+sudo apt install deluge remmina -y
 
 #
-sudo apt install sublime-text -y
+sudo apt install sublime-text timeshift -y
 
 #
 sudo apt install wireshark openssh-server -y
@@ -19,7 +19,7 @@ sudo apt install wireshark openssh-server -y
 sudo apt install code google-chrome-stable -y
 
 #tools
-sudo apt install minicom remmina remmina-plugin-vnc remmina-plugin-rdp aircrack-ng nmap gparted deluge aria2 pdfimages -y
+sudo apt install minicom aircrack-ng gparted aria2 pdfimages -y
 
 #virtualization
 sudo apt install virtualbox qemu -y
@@ -31,4 +31,4 @@ sudo apt install blender sweethome3d sweethome3d-furniture inkscape krita -y
 sudo apt install vlc kazam kdenlive ffmpeg -y
 
 #gnome
-sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu gnome-shell-extensions -y
+#sudo apt install gnome-tweaks materia-gtk-theme gnome-shell-extension-dash-to-panel gnome-shell-extension-arc-menu gnome-shell-extensions -y
