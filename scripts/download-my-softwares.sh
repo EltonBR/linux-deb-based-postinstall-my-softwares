96006ea38ddbd9f4ab2756cb8562c06e5e0841e8
Elton John <37675998+EltonBR@users.noreply.github.com>
2023-03-08 12:34:10 -0300

    Update download-my-softwares.sh

--- scripts/download-my-softwares.sh
+++ scripts/download-my-softwares.sh
@@ -4,23 +4,13 @@ sudo apt install gitk meld build-essential cmake make -y
 
 #cli tools
 sudo apt install ranger ncdu curl net-tools nmap tmux ne -y
-sudo apt install emacs -y
 #utils
-#sudo apt install filezilla -y
 sudo apt install deluge remmina -y
+sudo apt install timeshift -y
 
-#
-sudo apt install sublime-text timeshift -y
-
-sudo apt install merkaator marble osmctools -y
-#
-sudo apt install wireshark openssh-server -y
-
-sudo apt install gnuradio -y
-sudo apt install nfs-common -y
 sudo apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev -y
-
-
+sudo apt install wireshark openssh-server -y
+sudo apt install nfs-common -y
 #
 sudo apt install code google-chrome-stable -y
 
@@ -50,7 +40,6 @@ sudo apt install rtl-sdr -y
 sudo apt install librtlsdr-dev -y
 
 #DEV LIBS
-
 sudo apt install libfftw3-dev -y
 sudo apt install libncurses-dev -y
 #gnome
